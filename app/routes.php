249822d19c32c4d7f8d7a560edf69c85e1c62fc2
Гуцалов Сергей <gucalovsergej@rambler.ru>
2025-09-18 21:20:30 +0300

    Добавить группу маршрутов для профиля с маршрутом для получения списка пользователей

--- app/routes.php
+++ app/routes.php
@@ -25,6 +25,10 @@ return function (App $app) {
         ]);
     });
 
+    $app->group('/profile', function (Group $group) {
+        $group->get('', ListUsersAction::class);
+    });
+
     $app->group('/users', function (Group $group) {
         $group->get('', ListUsersAction::class);
         $group->get('/{id}', ViewUserAction::class);
