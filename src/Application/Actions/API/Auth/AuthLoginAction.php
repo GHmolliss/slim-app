5a0213af5f11468376ee674921189c9eb9f291b5
Гуцалов Сергей <gucalovsergej@rambler.ru>
2025-09-20 16:03:57 +0300

    Добавить статус ответа 201 при успешном входе в систему в AuthLoginAction

--- src/Application/Actions/API/Auth/AuthLoginAction.php
+++ src/Application/Actions/API/Auth/AuthLoginAction.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Application\Actions\API\Auth;
 
 use App\Interface\API\Auth\Login\ApiLoginRequestDto;
+use Fig\Http\Message\StatusCodeInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 
 class AuthLoginAction extends AuthAction
@@ -15,6 +16,9 @@ class AuthLoginAction extends AuthAction
 
         $responseDto = $this->apiLoginInterface->get($requestDto);
 
-        return $this->respondWithData($responseDto->toArray());
+        return $this->respondWithData(
+            $responseDto->toArray(),
+            StatusCodeInterface::STATUS_CREATED,
+        );
     }
 }
