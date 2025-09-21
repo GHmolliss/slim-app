d57400aa7d6b2dd282f3ee6876de59ffd8de33d5
Гуцалов Сергей <gucalovsergej@rambler.ru>
2025-09-21 10:31:53 +0300

    Добавить функцию создания Slim приложения с настройкой контейнера, зависимостей, middleware и маршрутов

--- tests/Functional/bootstrap.php
+++ tests/Functional/bootstrap.php
@@ -1,3 +1,39 @@
+
 <?php
 
 require __DIR__ . '/../../vendor/autoload.php';
+
+use Slim\App;
+use Slim\Factory\AppFactory;
+use DI\ContainerBuilder;
+
+function createSlimApp(): App
+{
+    $containerBuilder = new ContainerBuilder();
+
+    // Set up settings
+    $settings = require __DIR__ . '/../../app/settings.php';
+    $settings($containerBuilder);
+
+    // Set up dependencies
+    $dependencies = require __DIR__ . '/../../app/dependencies.php';
+    $dependencies($containerBuilder);
+
+    $container = $containerBuilder->build();
+    AppFactory::setContainer($container);
+    $app = AppFactory::create();
+
+    // Register middleware
+    $middleware = require __DIR__ . '/../../app/middleware.php';
+    $middleware($app);
+
+    // Register routes
+    $routes = require __DIR__ . '/../../app/routes.php';
+    $routes($app);
+
+    // Register API routes
+    $routes = require __DIR__ . '/../app/routes_api.php';
+    $routes($app);
+
+    return $app;
+}
